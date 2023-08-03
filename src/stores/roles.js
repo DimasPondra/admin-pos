eedfbfa19a4707dd2e1930d7b3afbb64523969cf
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2023-08-03 12:23:05 +0700

    add filter, component, and handle update, delete

--- src/stores/roles.js
+++ src/stores/roles.js
@@ -1,62 +1,118 @@
 import { defineStore } from "pinia";
 import { reactive } from "vue";
 import { useAuthStore } from "./auth";
+import { useAlertStore } from "./alerts";
 import axios from "axios";
 import router from "../router";
 
 export const useRoleStore = defineStore("role", () => {
     const data = reactive({
+        roles: [],
         role: {
             id: null,
             name: "",
             slug: "",
         },
-        roles: [],
+        pagination: {
+            page: 1,
+            total: 0,
+            per_page: 10,
+            option: {
+                chunk: 3,
+                chunksNavigation: "scroll",
+                hideCount: true,
+            },
+        },
     });
 
-    const auth = useAuthStore();
+    const authStore = useAuthStore();
+    const alertStore = useAlertStore();
 
-    async function get() {
+    const get = async (params) => {
+        clear();
+
         try {
-            const response = await axios.get("admin/roles", {
+            const res = await axios.get("admin/roles", {
+                params: params,
                 headers: {
-                    Authorization: auth.token,
+                    Authorization: authStore.token,
                 },
             });
 
-            data.roles = response.data.data;
+            data.roles = res.data.data;
+
+            if (res.data.meta != null) {
+                data.pagination.page = res.data.meta.current_page;
+                data.pagination.total = res.data.meta.total;
+                data.pagination.per_page = res.data.meta.per_page;
+            }
         } catch (error) {
-            console.log(error);
+            alertStore.handleError(error);
         }
-    }
+    };
 
-    async function show(id) {
+    const show = async (id) => {
+        clear();
+
         try {
-            const response = await axios.get(`admin/roles/${id}/show`, {
+            const res = await axios.get(`admin/roles/${id}/show`, {
                 headers: {
-                    Authorization: auth.token,
+                    Authorization: authStore.token,
                 },
             });
 
-            data.role = response.data.data;
+            data.role = res.data.data;
         } catch (error) {
-            console.log(error);
+            alertStore.handleError(error);
         }
-    }
+    };
 
-    async function save(data) {
+    const save = async (data, id) => {
         try {
-            await axios.post("admin/roles/store", data, {
-                headers: {
-                    Authorization: auth.token,
-                },
-            });
+            if (id == null) {
+                await axios.post("admin/roles/store", data, {
+                    headers: {
+                        Authorization: authStore.token,
+                    },
+                });
 
+                alertStore.handleSuccess("successfully created.");
+            } else {
+                await axios.patch(`admin/roles/${id}/update`, data, {
+                    headers: {
+                        Authorization: authStore.token,
+                    },
+                });
+
+                alertStore.handleSuccess("successfully updated.");
+            }
+
+            clear();
             router.push("/roles");
         } catch (error) {
-            console.log(error);
+            alertStore.handleError(error);
         }
-    }
+    };
 
-    return { data, get, show, save };
+    const deleteItem = async (id) => {
+        try {
+            await axios.delete(`admin/roles/${id}/delete`, {
+                headers: {
+                    Authorization: authStore.token,
+                },
+            });
+
+            alertStore.handleSuccess("successfully deleted.");
+        } catch (error) {
+            alertStore.handleError(error);
+        }
+    };
+
+    const clear = () => {
+        data.role.id = null;
+        data.role.name = "";
+        data.role.slug = "";
+    };
+
+    return { data, get, show, save, deleteItem };
 });
