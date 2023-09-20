5b2b475a8e57bc1be555bc31fcdc78b5562c9d4b
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2023-09-20 10:33:58 +0700

    :sparkles: add filter and download PDF

--- src/components/TittlePage.vue
+++ src/components/TittlePage.vue
@@ -5,6 +5,9 @@
             <router-link :to="title.link_create" class="btn btn-sm btn-primary"
                 >Add new {{ titleLowerCase }}</router-link
             >
+            <button v-if="title.generate == true" @click="handleGeneratePDF" class="btn btn-sm btn-warning ms-2">
+                Generate PDF
+            </button>
         </div>
     </div>
 </template>
@@ -19,10 +22,15 @@ export default {
             default: () => {},
         },
     },
-    setup(props) {
+    emits: ["handleGeneratePDF"],
+    setup(props, { emit }) {
         const titleLowerCase = computed(() => props.title.name.toLowerCase());
 
-        return { titleLowerCase };
+        const handleGeneratePDF = () => {
+            emit("handleGeneratePDF");
+        };
+
+        return { titleLowerCase, handleGeneratePDF };
     },
 };
 </script>
